32129a89c9e4a5dae9589e6888dc00c95f932d8b
Joseph Marchbanks <jmarchbanks2@cnm.edu>
2024-03-12 15:19:58 -0600

    Changed out picture in about section

--- script.js
+++
@@ -1,9 +0,0 @@
-// Get the follower element
-const follower = document.getElementById('follower');
-
-// Add mousemove event listener to the document
-document.addEventListener('mousemove', (event) => {
-    // Update follower position to match mouse cursor
-    follower.style.left = event.pageX + 'px'
-    follower.style.top = event.pageY + 'px'
-})
